89682159022a3a9aacf5546482d7a9e8aa6a3cdf
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-08-08 20:28:56 +0530

    Add files via upload

---
+++ modules/main_module/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+module "main_module"  {
+    source = "./Create_Bucket"
+    s3_buckets_names = var.s3_buckets_names
+    
+}
+
+module "create_users"  {
+    source = "./Create_User"
+    iam_name = var.iam_name
+}
+
+module "create_roles"  {
+    source = "./Create_Role_Assign_Roles"
+    iam_policy_names = var.iam_policy_names
+    iam_name = var.iam_name
+}
