389dd586c9bc6c89fe1139d1e84b415f4de6d26a
amitsharma <55508548+amit17133129@users.noreply.github.com>
2022-08-18 09:19:35 +0530

    Update s3.tf

--- modules/main_module/Create_Bucket/s3.tf
+++ modules/main_module/Create_Bucket/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket"  "my_bucks" {
-  count  = length(var.s3_buckets_names)
-  bucket = var.s3_buckets_names[count.index]
+#   count  = length(var.s3_buckets_names)
+  bucket = "tyuioiuhgfhjjhbvnmjnuu"
   acl    = "private"
 }
 
